7c86f0781f1a12e0a1080c76f582bf58ffd9c359
liupiaoyi <liupiaoyi1024@sina.com>
2017-11-25 15:27:38 +0800

    little change

--- src/main/java/FibonacciSupplier.java
+++ src/main/java/FibonacciSupplier.java
@@ -3,6 +3,7 @@ package main.java;
 import java.util.function.Supplier;
 
 public class FibonacciSupplier implements Supplier<Long> {
+	//f(n) = f(n-1)+f(n-2)+f(n-3)
   private long longBef = 1;
   private long before = 2;
   private long now = 4;
